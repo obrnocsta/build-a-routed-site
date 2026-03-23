d60e0f42030d6d7e583d67ed24d1d6d6084ae922
Bruno Costa <obrnocsta@gmail.com>
2026-03-23 02:42:57 -0300

    feat: adiciona rota 'GET' para '/api/news' usando 'Serve-Sent Event' para publicar novas notícias de './data/store.js'

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -4,6 +4,7 @@ import { addData } from "../utils/addData.js";
 import { getData } from "../utils/getData.js";
 import { sanitizeData } from "../utils/sanitizeData.js";
 import { event } from "../events/events.js";
+import { stories } from "../data/stories.js";
 
 const data = await getData();
 // handleGet
@@ -36,3 +37,26 @@ export const handlePost = async (req, res) => {
 // - get our existing data
 // - add the new data to the existing data
 // - write the completed data to the JSON file
+
+export const handleNews = async (req, res) => {
+  res.statusCode = 200;
+
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * stories.length);
+
+    res.write(
+      `data: ${JSON.stringify({
+        event: "news-update",
+        story: stories[randomIndex],
+      })}\n\n`,
+    );
+  }, 3000);
+};
+
+// The object should include:
+// - an event property with a descriptive name.
+// - a story chosen at random from the stories array.
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from "node:http";
 import { serverStatic } from "./utils/serverStatic.js";
-import { handleGet, handlePost } from "./handlers/routeHandlers.js";
+import { handleGet, handlePost, handleNews } from "./handlers/routeHandlers.js";
 
 const PORT = 8000;
 
@@ -8,14 +8,20 @@ const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
-  if (url.pathname === "/api") {
-    if (req.method === "GET") {
-      return await handleGet(req, res);
-    }
-    if (req.method === "POST") {
-      return await handlePost(req, res);
-    }
-  } else {
+
+  if (url.pathname === "/api" && req.method === "GET") {
+    return await handleGet(req, res, __dirname);
+  }
+
+  if (url.pathname === "/api" && req.method === "POST") {
+    return await handlePost(req, res);
+  }
+
+  if (url.pathname === "/api/news" && req.method === "GET") {
+    return await handleNews(req, res);
+  }
+
+  if (!url.pathname.startsWith("/api")) {
     return await serverStatic(req, res, __dirname);
   }
 });
